315165c5cb34da05e5107fc4267e3048ddd7f0c9
gawema <gab.mannu@hotmail.it>
2021-02-06 19:20:53 +0100

    adjusted some styling

--- src/screens/Meals.js
+++ src/screens/Meals.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './Meals.css'
+import Button from '../components/Button'
+import { useHistory } from "react-router-dom";
 import OrderStatusBox from '../components/OrderStatusBox'
 
 const Meals = () => {
+	const history = useHistory();
+
 
 	return (
 		<>
-			<div className="meal box">
-				<img src={null} alt="Meals" />
-				<div className="meal-description"></div>
+			<div className="meal">
+				<img src={null} alt="Meals" className="box"/>
+				<div className="meal-description box"></div>
+				<Button command="GENERATE NEW" onClick={() => {
+					history.push({
+						pathname: "/",
+					})
+				}}
+				style={{
+					margin: "20px 0 0 auto"
+				}}/>
 			</div>
 			<OrderStatusBox command="NEXT" nextStep="select-drinks"/>
 		</>
