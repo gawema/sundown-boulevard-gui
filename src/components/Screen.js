f0490a55837272364c8c17089adff379e0e4d4f6
gawema <gab.mannu@hotmail.it>
2021-02-06 16:42:57 +0100

    set screens based on state

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,19 +1,22 @@
-import React from 'react';
-// import Home from './Home';
-// import Meals from './Meals';
-// import Drinks from '../screens/Drinks';
-// import Order from '../screens/Order';
+import React, {useState} from 'react';
+import Home from '../screens/Home';
+import Meals from '../screens/Meals';
+import Drinks from '../screens/Drinks';
+import Order from '../screens/Order';
 import Receipt from '../screens/Receipt';
 import './Screen.css'
 
 const Screen = () => {
 
+	const [openScreen, setOpenScreen] = useState('home');
+
 	return (
 		<div className="screen-container">
-			{/* <Home /> */}
-			{/* <Meals /> */}
-			{/* <Order /> */}
-			<Receipt />
+			{openScreen === 'home' && <Home />}
+			{openScreen === 'meals' && <Meals />}
+			{openScreen === 'order' && <Order />}
+			{openScreen === 'drinks' && <Drinks />}
+			{openScreen === 'receipt' && <Receipt />}
 		</div>
 	);
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
 	return (
 		<>
 			<div className="slideshow box"></div>
-			<div className="order box"></div>
+			<div className="neworder box"></div>
 			<div className="find box"></div>
 			<div className="content box"></div>
 		</>
