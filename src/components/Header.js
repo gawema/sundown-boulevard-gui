f2542e95fa0c7ea3f176d86eb9d819532e3124c5
gawema <gab.mannu@hotmail.it>
2021-02-07 22:12:41 +0100

    added home link to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,14 @@ const Header = () => {
 
 	return (
 		<div className="header primary-font-color">
-			<img src={logo} height={60} alt="logo"/>
+			<img src={logo} height={60} alt="logo" 
+				onClick={() => {
+					window.location.href = '/';
+				}}
+				style={{
+					cursor: "pointer"
+				}}
+			/>
 			<a href> RESTAURANTER </a>
 			<a href> PRODUKTER </a>
 			<a href> NYHEDSBREV </a>
