74b13608ed5601ded9132298f3ded5fa79ba3468
gawema <gab.mannu@hotmail.it>
2021-02-07 16:03:35 +0100

    added ability to add and remove drinks

--- src/screens/Drinks.js
+++ src/screens/Drinks.js
@@ -1,28 +1,58 @@
 import React, {useEffect, useState} from 'react';
 import './Drinks.css'
 import OrderStatusBox from '../components/OrderStatusBox'
+import { useHistory } from "react-router-dom";
+
 
 const Drinks = () => {
 
-	const [drinks, setDrinks] = useState([])
+	const history = useHistory();
+
+	const [order, setOrder] = useState(JSON.parse(localStorage.getItem('order')))
+	const [allDrinks, setAllDrinks] = useState([])
 
 	useEffect(() => {
+		if(!order){
+			history.push({
+				pathname: '/',
+			})
+		}
 		fetch('https://api.punkapi.com/v2/beers')
 		.then(response => response.json())
-		.then(data => setDrinks(data))
+		.then(data => setAllDrinks(data))
+		console.log(order?.drinks)
 	},[])
+
+
+	useEffect(() => {
+		localStorage.setItem('order', JSON.stringify(order))
+	},[order])
+
+
+	const updateOrder = (id) => {
+		let newSelectDrinks = [...order.drinks]
+		const index = newSelectDrinks.indexOf(id)
+		if (index === -1){
+			newSelectDrinks.push(id)
+		}else{
+			newSelectDrinks.splice(index, 1)
+		}
+		setOrder({ ...order, drinks: [...newSelectDrinks]})
+	}
 
 	return (
 		<>
-			<div className="driks box">
-				{drinks.map(drink => {
+			<div className="drinks box">
+				{allDrinks.map(drink => {
 					return(
-					<div className="drink-card box" style={{
-						backgroundImage: `url(${drink.image_url})`
-					}}>
-						<div className="backdrop"></div>
-						<h4>{drink.name}</h4>
-					</div>)
+						<div className="drink-card box" 
+							style={{
+								backgroundImage: `url(${drink.image_url})`,
+								borderColor: `${order?.drinks?.indexOf(drink.id) !== -1 && order?.drinks ? '#BA2329' : '#333'}`	
+							}} onClick={() => updateOrder(drink.id)}>
+							<div className="backdrop"></div>
+							<h4>{drink.name}</h4>
+						</div>)
 				})}
 			</div>
 			<OrderStatusBox command="NEXT" nextStep="place-order"/>
